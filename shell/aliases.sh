53a26b7af7ec6e6134a35c295aa16bd81a08e61f
Leonardo Rodrigues Ferreira <leo.rf.developer@gmail.com>
2025-01-25 17:22:15 -0300

    Remove script that was not allowing to open files directly with editor

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -4,7 +4,3 @@ alias mkdir="mkdir -p"
 # Protect against overwriting
 alias mv="mv -i"
 alias cp="cp -i"
-
-# If inside tmux session, onle opens nvim, otherwise, open a new tmux-session with nvim
-alias nvim="([ -z "$TMUX" ] && tmux new-session nvim) ||  nvim"
-alias vim="nvim"
